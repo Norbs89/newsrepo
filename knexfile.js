c45703ba82b9430c79c465b8a0db4aaf826efa15
Norbs89 <n_huvos@live.co.uk>
2020-02-24 13:01:51 +0000

    added migration for topics table

--- knexfile.js
+++ knexfile.js
@@ -1,27 +1,27 @@
-const ENV = process.env.NODE_ENV || 'development';
+const ENV = process.env.NODE_ENV || "development";
 
 const baseConfig = {
-  client: 'pg',
+  client: "pg",
   migrations: {
-    directory: './db/migrations'
+    directory: "./db/migrations"
   },
   seeds: {
-    directory: './db/seeds'
+    directory: "./db/seeds"
   }
 };
 
 const customConfig = {
   development: {
     connection: {
-      database: 'nc_news'
+      database: "nc_news",
       username: "norbs89",
       password: "password"
     }
   },
   test: {
     connection: {
-      database: 'nc_news_test'
-     username: "norbs89",
+      database: "nc_news_test",
+      username: "norbs89",
       password: "password"
     }
   }
