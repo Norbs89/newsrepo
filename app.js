121057be89bb5f131164baa83848a70e3736a9e7
Norbs89 <n_huvos@live.co.uk>
2020-02-28 14:30:49 +0000

    minor bug fixes

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const apiRouter = require("./routers/api.router.js");
-const { customErrors, send404Error, pSQLErrors } = require("./errors/errors");
+const {
+  customErrors,
+  send404Error,
+  pSQLErrors,
+  send405Error
+} = require("./errors/errors");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api", apiRouter);
+
+app.use("/*", send405Error);
 
 app.use(customErrors);
 app.use(pSQLErrors);
