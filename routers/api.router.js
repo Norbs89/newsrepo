6bbe3fc174e00909a42cf1c0e42914c0c4192dc8
Norbs89 <n_huvos@live.co.uk>
2020-02-28 15:28:35 +0000

    minor bug fix

--- routers/api.router.js
+++ routers/api.router.js
@@ -11,7 +11,10 @@ apiRouter.use("/users", usersRouter);
 apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/articles", articlesRouter);
 apiRouter.use("/comments", commentsRouter);
-apiRouter.get("/", sendEndPoints).all(send405Error);
+apiRouter
+  .route("/")
+  .get(sendEndPoints)
+  .all(send405Error);
 apiRouter.use("/*", send404Error);
 apiRouter.all(send405Error);
 
